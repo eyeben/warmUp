212e4501e8e3e53c28da24ca0124d82a4b488381
박동민 <qkrehdals97@naver.com>
2024-01-05 17:45:33 +0900

    initial commit

---
+++ src/main/java/softeerWarmup1/demo/Warmup1Application.java
@@ -0,0 +1,13 @@
+package softeerWarmup1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Warmup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Warmup1Application.class, args);
+	}
+
+}
